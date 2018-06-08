5fe4ea44cf5571d7848a858c4889d4e1d6e94038
Ran Shoham <my.happy.porcupine@gmail.com>
2018-06-09 00:47:32 +0300

    Make comments appear with time automatically

--- main.js
+++ main.js
@@ -16,4 +16,33 @@ function incrementNumber() {
 	setNumber(newNumber);
 }
 
-setInterval(incrementNumber, 1000);
+function addComment(commentString) {
+	const commentParagraph = document.createElement('p');
+	commentParagraph.textContent = commentString;
+	const commentsDiv = document.getElementById('comments');
+	commentsDiv.appendChild(commentParagraph);
+}
+
+const comments = [
+	{text: 'This counts numbers...', time: 5},
+	{text: 'Look at it go!', time: 12},
+	{text: 'What do you think it\'s counting?', time: 22},
+	{text: 'What?', time: 28},
+];
+
+function timeComments() {
+	const time = getNumber();
+	for (comment of comments) {
+		if (comment.time == time) {
+			const commentText = comment.text;
+			addComment(commentText);
+		}
+	}
+}
+
+function step() {
+	incrementNumber();
+	timeComments();
+}
+
+setInterval(step, 1000);
